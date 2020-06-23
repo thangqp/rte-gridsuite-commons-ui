f336d7fee1788a6aadcdf8da5414c36a02aa6304
Mika Théodore <michael.theodore@rte-france.com>
2020-06-23 21:21:51 +0200

    First version of commons-ui  (#2)
    Signed-off-by: ChamseddineBhd <chamseddine.benhamed@ensi-uma.tn>

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# common-components
+# commons-ui
+
+Library for sharing GridSuite apps commons components
+
+###For developpers
+If you want to test the library usage in a consumer application my-app
+change the dependency from `@gridsuite/commons-ui:'^x.x.x'` to  `@gridsuite/commons-ui:'file:../path/to/the/commons-ui'` 
+then build the commons-ui `npm install` `npm run build`
+then in the root folder of my-app `npm install` `npm start`.
+
+**Note** : be sure that peer dependencies are not installed in the library node-modules to avoid problem such as duplicated react instances
---
+++ demo/src/index.js
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import React from 'react'
+import {render} from 'react-dom'
+
+import TopBar from '../../src/components/TopBar'
+
+import {createMuiTheme, ThemeProvider} from '@material-ui/core/styles';
+
+const lightTheme = createMuiTheme({
+    palette: {
+        type: 'light',
+    }
+});
+
+const darkTheme = createMuiTheme({
+    palette: {
+        type: 'dark',
+    }
+});
+
+const Demo = () => {
+    return (
+        <div>
+            <ThemeProvider theme={lightTheme}>
+                    <TopBar appName="StudyGrid" onParametersClick={() => console.log("settings")} onLogoutClick={() => console.log("logout")} onLogoClick={() => console.log("logo")} user={{profile : {name : "John Doe"}}} />
+            </ThemeProvider>
+        </div>
+  )
+};
+
+render(<Demo/>, document.querySelector('#demo'));
