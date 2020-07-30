5a1905cc9f0b94ec7dfd69290199eebcbedeebda
chamseddine <chamseddine.benhamed@ensi-uma.tn>
2020-07-30 18:03:48 +0200

    remove useless exports
    Signed-off-by: chamseddine <chamseddine.benhamed@ensi-uma.tn>

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
-import React from 'react'
-
 export TopBar from './components/TopBar'
-export Login from './components/Login'
-export SignInCallbackHandler from './components/SignInCallbackHandler'
-export SilentRenewCallbackHandler from './components/SilentRenewCallbackHandler'
 export AuthenticationRouter from './components/AuthenticationRouter'
 
-export UserManagerMock from './utils/UserManagerMock'
-export {initializeAuthentication, handleSilentRenewCallback, login, logout, dispatchUser, handleSigninCallback, getPreLoginPath} from './utils/AuthService'
+export {initializeAuthentication, logout, dispatchUser, getPreLoginPath} from './utils/AuthService'
 export {USER, setLoggedUser, SIGNIN_CALLBACK_ERROR, setSignInCallbackError} from './utils/actions'
