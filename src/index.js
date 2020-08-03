5e438060743846fc49ded6dc47ebd8b45253c478
chamseddine <chamseddine.benhamed@ensi-uma.tn>
2020-08-03 15:24:45 +0200

    typo
    Signed-off-by: chamseddine <chamseddine.benhamed@ensi-uma.tn>

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ export AuthenticationRouter from './components/AuthenticationRouter'
 
 export {initializeAuthenticationDev, initializeAuthenticationProd, logout, dispatchUser, getPreLoginPath} from './utils/AuthService'
 export {USER, setLoggedUser, SIGNIN_CALLBACK_ERROR, setSignInCallbackError} from './utils/actions'
-export logon_en from './components/translations/login-en'
+export login_en from './components/translations/login-en'
 export login_fr from './components/translations/login-fr'
 export top_bar_en from './components/translations/top-bar-en'
 export top_bar_fr from './components/translations/top-bar-fr'
