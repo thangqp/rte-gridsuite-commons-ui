8435bfd04a1aa2040ae6ae94a61b3fb3952932a4
chamseddine <chamseddine.benhamed@ensi-uma.tn>
2020-08-05 11:03:15 +0200

    small fixes
    Signed-off-by: chamseddine <chamseddine.benhamed@ensi-uma.tn>

--- demo/src/index.js
+++ demo/src/index.js
@@ -4,7 +4,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+
+import React from 'react'
+import {render} from 'react-dom'
+
 import App from "./app"
-import React from "react";
 
 render(<App/>, document.querySelector('#demo'));
