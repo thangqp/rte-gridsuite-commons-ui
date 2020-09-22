5ea83994cd3f4ecac53c7c86a60c76bfde1315e8
Jon Harper <jon.harper87@gmail.com>
2020-09-22 13:12:24 +0200

    phantomjs -> chromeheadless to support const
    Signed-off-by: Jon Harper <jon.harper87@gmail.com>

--- nwb.config.js
+++ nwb.config.js
@@ -1,5 +1,10 @@
+process.env.CHROME_BIN = require('puppeteer').executablePath()
+
 module.exports = {
   type: 'react-component',
+  karma: {
+    browsers: ["ChromeHeadless"]
+  },
   npm: {
     esModules: false,
     umd: false
