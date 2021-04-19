4ebd8827f58e4ba8fc2ad117d309b6101846c73d
etiennehomer <etiennehomer@gmail.com>
2021-04-19 09:54:54 +0200

    Disable parameters button when callback is undefined (#51)
    Signed-off-by: HOMER Etienne <etiennehomer@gmail.com>

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -450,7 +450,7 @@ const TopBar = ({
                                         </StyledMenuItem>
 
                                         {/*/!* Equipment labelling *!/*/}
-                                        {/*If the callback onEquipmentLabellingClick is not defined, equipment labelling component isn't displayed*/}
+                                        {/*If the callback onEquipmentLabellingClick is undefined, equipment labelling component should not be displayed*/}
                                         {onEquipmentLabellingClick && (
                                             <StyledMenuItem
                                                 disabled={true}
@@ -516,7 +516,9 @@ const TopBar = ({
                                         )}
 
                                         {/* Settings */}
+                                        {/*If the callback onParametersClicked is undefined, parameters component should be disabled*/}
                                         <StyledMenuItem
+                                            disabled={!onParametersClick}
                                             onClick={onParametersClicked}
                                             className={classes.borderTop}
                                         >
