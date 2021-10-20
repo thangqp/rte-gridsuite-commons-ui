f69cfe9c9d79946ce819c35c4ce1f56c709c7417
niconoir <47850973+niconoir@users.noreply.github.com>
2021-10-20 13:05:32 +0200

    Add possibility to click or not the logo in the TopBar (#86)
    Signed-off-by: NOIR Nicolas ext <nicolas.noir@rte-france.com>

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -45,6 +45,7 @@ import Popper from '@material-ui/core/Popper';
 import Paper from '@material-ui/core/Paper';
 import MenuList from '@material-ui/core/MenuList';
 import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import clsx from 'clsx';
 
 const useStyles = makeStyles((theme) => ({
     grow: {
@@ -55,7 +56,6 @@ const useStyles = makeStyles((theme) => ({
         flexShrink: 0,
         width: 48,
         height: 48,
-        cursor: 'pointer',
         marginBottom: 8,
     },
     menuIcon: {
@@ -64,6 +64,8 @@ const useStyles = makeStyles((theme) => ({
     },
     title: {
         marginLeft: 18,
+    },
+    clickable: {
         cursor: 'pointer',
     },
     link: {
@@ -270,12 +272,19 @@ const TopBar = ({
                 }
             />
             <Toolbar>
-                <div className={classes.logo} onClick={onLogoClick}>
+                <div
+                    className={clsx(classes.logo, {
+                        [classes.clickable]: onLogoClick,
+                    })}
+                    onClick={onLogoClick}
+                >
                     {appLogo}
                 </div>
                 <Typography
                     variant="h4"
-                    className={classes.title}
+                    className={clsx(classes.title, {
+                        [classes.clickable]: onLogoClick,
+                    })}
                     onClick={onLogoClick}
                 >
                     <span style={{ fontWeight: 'bold' }}>Grid</span>
