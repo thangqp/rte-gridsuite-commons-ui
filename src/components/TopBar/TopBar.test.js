cff336122f3e122bfd739542d1a0809899813618
HARPER Jon <jon.harper87@gmail.com>
2023-01-04 14:55:05 +0100

    use testing-library

--- src/components/TopBar/TopBar.test.js
+++ src/components/TopBar/TopBar.test.js
@@ -7,8 +7,7 @@
 
 import expect from 'expect';
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { act } from 'react-dom/test-utils';
+import { render } from '@testing-library/react';
 import { IntlProvider } from 'react-intl';
 
 import TopBar from './TopBar';
@@ -18,19 +17,6 @@ import PowsyblLogo from '../images/powsybl_logo.svg';
 
 import { red } from '@mui/material/colors';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-
-let container = null;
-beforeEach(() => {
-    // setup a DOM element as a render target
-    container = document.createElement('div');
-    document.body.appendChild(container);
-});
-
-afterEach(() => {
-    // cleanup on exiting
-    container.remove();
-    container = null;
-});
 
 const apps = [
     { name: 'App1', url: '/app1', appColor: 'blue', hiddenInAppsMenu: false },
@@ -46,29 +32,23 @@ const theme = createTheme({
 });
 
 it('renders', () => {
-    const root = createRoot(container);
-    act(() => {
-        root.render(
-            <ThemeProvider theme={theme}>
-                <IntlProvider locale="en" messages={top_bar_en}>
-                    <TopBar
-                        appName="Demo"
-                        appColor="#808080"
-                        appLogo={PowsyblLogo}
-                        onParametersClick={() => {}}
-                        onLogoutClick={() => {}}
-                        onLogoClick={() => {}}
-                        user={{ profile: { name: 'John Doe' } }}
-                        appsAndUrls={apps}
-                    >
-                        <p>testchild</p>
-                    </TopBar>
-                </IntlProvider>
-            </ThemeProvider>
-        );
-    });
+    const { container } = render(
+        <ThemeProvider theme={theme}>
+            <IntlProvider locale="en" messages={top_bar_en}>
+                <TopBar
+                    appName="Demo"
+                    appColor="#808080"
+                    appLogo={PowsyblLogo}
+                    onParametersClick={() => {}}
+                    onLogoutClick={() => {}}
+                    onLogoClick={() => {}}
+                    user={{ profile: { name: 'John Doe' } }}
+                    appsAndUrls={apps}
+                >
+                    <p>testchild</p>
+                </TopBar>
+            </IntlProvider>
+        </ThemeProvider>
+    );
     expect(container.textContent).toContain('GridDemotestchildJD');
-    act(() => {
-        root.unmount();
-    });
 });
