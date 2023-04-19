93de1891ca6761634fd4b463adc4c3c235516e99
laurgarn <88483560+laurgarn@users.noreply.github.com>
2023-04-19 17:33:35 +0200

    More parameters types (#248)
    * feat FlatParameters detached from useImportExportParams + demo
    * feat CSS adjustments for FlatParameters, notably horiz margins to 0
    * removal of debugging code
    * fixrev: inline jsx at end of FlatParameters
    * fixrev: move new deps to dev dependencies
    * fixrev: right align numbers
    * fixrev: license headers + console.debug removals
    * fixrev: translation for single possibleValues too
    * fixrev: console warning about height of resizable box
    * fixrev: uses select for single choice out of possible values, as in gridstudy parameters
    * fixrev: remove unused code
    * fixrev: force curly braces for if and else
    * fixrev: console.debug removal
    
    * feat(): Add style variant for TextField, Autocomplete and Select parameter fields
    
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>
    
    * fix exhaustive deps
    
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>
    
    * chore(): rename fontab to defaultTab
    
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>
    
    * fixrev: better handling of empty double and NaN, show transitory bad format MUI Textfield error
    * fixrev: give a greater minimum width for select
    
    ---------
    
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>
    Co-authored-by: sBouzols <sylvain.bouzols@gmail.com>

---
+++ demo/src/FlatParametersTab.js
@@ -0,0 +1,125 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import { useImportExportParams } from '../../src';
+import React from 'react';
+import RightResizableBox from './right-resizable-box';
+import FlatParameters from '../../src/components/FlatParameters/FlatParameters';
+
+const EXAMPLE_PARAMETERS = [
+    {
+        name: 'voltageRemoteControl',
+        type: 'BOOLEAN',
+        description: 'Generator voltage remote control',
+        defaultValue: 'true',
+        possibleValues: null,
+    },
+    {
+        name: 'plausibleActivePowerLimit',
+        type: 'DOUBLE',
+        description: 'Plausible active power limit',
+        defaultValue: '5000.0',
+        possibleValues: null,
+    },
+    {
+        name: 'slackBusSelectionMode',
+        type: 'STRING',
+        description: 'Slack bus selection mode',
+        defaultValue: 'MOST_MESHED',
+        possibleValues: ['FIRST', 'MOST_MESHED', 'NAME', 'LARGEST_GENERATOR'],
+    },
+    {
+        name: 'limitReductions',
+        type: 'STRING_LIST',
+        description: 'Limit reductions (in JSON)',
+        defaultValue: [],
+        possibleValues: null,
+    },
+    {
+        name: 'slackBusesIds',
+        type: 'STRING',
+        description: 'Slack bus IDs',
+        defaultValue: 'null',
+        possibleValues: null,
+    },
+    {
+        name: 'maxIteration',
+        type: 'INTEGER',
+        description: 'Max iterations',
+        defaultValue: '30',
+        possibleValues: null,
+    },
+    {
+        name: 'newtonRaphsonConvEpsPerEq',
+        type: 'DOUBLE',
+        description: 'Newton-Raphson convergence epsilon per equation',
+        defaultValue: '1.0E-4',
+        possibleValues: null,
+    },
+    {
+        name: 'iidm.export.cgmes.base-name',
+        type: 'STRING',
+        description: 'Basename for output files',
+        defaultValue: null,
+        possibleValues: null,
+    },
+    {
+        name: 'iidm.export.cgmes.cim-version',
+        type: 'STRING',
+        description: 'CIM version to export',
+        defaultValue: null,
+        possibleValues: ['14', '16', '100'],
+    },
+    {
+        name: 'iidm.export.cgmes.export-boundary-power-flows',
+        type: 'BOOLEAN',
+        description: "Export boundaries' power flows",
+        defaultValue: true,
+        possibleValues: null,
+    },
+    {
+        name: 'iidm.export.cgmes.export-power-flows-for-switches',
+        type: 'BOOLEAN',
+        description: 'Export power flows for switches',
+        defaultValue: false,
+        possibleValues: null,
+    },
+    {
+        name: 'iidm.export.cgmes.naming-strategy',
+        type: 'STRING',
+        description: 'Configure what type of naming strategy you want',
+        defaultValue: 'identity',
+        possibleValues: ['identity', 'cgmes', 'cgmes-fix-all-invalid-ids'],
+    },
+    {
+        name: 'iidm.export.cgmes.profiles',
+        type: 'STRING_LIST',
+        description: 'Profiles to export',
+        defaultValue: ['EQ', 'TP', 'SSH', 'SV'],
+        possibleValues: ['EQ', 'TP', 'SSH', 'SV'],
+    },
+];
+
+export const FlatParametersTab = () => {
+    const [currentParameters1, paramsComponent1] = useImportExportParams(
+        EXAMPLE_PARAMETERS,
+        null,
+        false
+    );
+    return (
+        <div style={{ display: 'flex', margin: 8 }}>
+            <RightResizableBox>{paramsComponent1}</RightResizableBox>
+            <RightResizableBox>
+                <FlatParameters
+                    paramsAsArray={EXAMPLE_PARAMETERS}
+                    initValues={currentParameters1}
+                    variant={'standard'}
+                />
+            </RightResizableBox>
+        </div>
+    );
+};
