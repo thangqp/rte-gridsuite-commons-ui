7109458997b6863ebe80bb081a0e8f7b31b2cc29
Tristan Chuine <tristan.chuine_externe@rte-france.com>
2023-11-16 10:34:13 +0100

    Update float regex validation

--- src/components/react-hook-form/numbers/utils.js
+++ src/components/react-hook-form/numbers/utils.js
@@ -1,7 +1,7 @@
 export const isIntegerNumber = (val) => {
-    return /^-?[0-9]*$/.test(val);
+    return /^-?[0-9]+$/.test(val);
 };
 
 export const isFloatNumber = (val) => {
-    return /^-?[0-9]*[.,]?[0-9]*$/.test(val);
+    return /^-?(?:[0-9]+(?:[.,][0-9]*)?|[.][0-9]+)(?:[eE]-?[0-9]+)?$/.test(val);
 };
