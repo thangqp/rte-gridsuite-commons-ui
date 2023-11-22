0f811fb234b5532fd56dbdc7b8be9799fd487be6
Thang PHAM <phamthang37@gmail.com>
2023-11-22 16:01:43 +0100

    Limit exponential part to -+20

--- src/components/react-hook-form/numbers/utils.js
+++ src/components/react-hook-form/numbers/utils.js
@@ -2,6 +2,7 @@ export const isIntegerNumber = (val) => {
     return /^-?[0-9]*$/.test(val);
 };
 
+// litmit the exponential part to two digits and <= 20 ([0-9]|1[0-9]|20)?
 export const isFloatNumber = (val) => {
-    return /^-?[0-9]*[.,]?[0-9]*([eE][-+]?[0-9]*)?$/.test(val);
+    return /^-?[0-9]*[.,]?[0-9]*([eE][-+]?([0-9]|1[0-9]|20)?)?$/.test(val);
 };
