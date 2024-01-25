19c46eb8d3db9a45939ac3ba8fee50acb9f47329
Florent MILLOT <millotflo@gmail.com>
2024-01-25 18:29:36 +0100

    rename index
    Signed-off-by: Florent MILLOT <millotflo@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
     build: {
         lib: {
             // Could also be a dictionary or array of multiple entry points
-            entry: resolve(__dirname, 'src/index.jsx'),
+            entry: resolve(__dirname, 'src/index.js'),
             name: 'Commons ui',
             // the proper extensions will be added
             fileName: 'commons-ui',
