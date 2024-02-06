cce9e277be71f1004c725ea6c10121d783b186c6
Florent MILLOT <millotflo@gmail.com>
2024-02-06 16:19:54 +0100

    Update package.json version to 0.43.0, change tsconfig.json module to "esnext", and update darken import from TopBar.jsx. Add headers.
    Signed-off-by: Florent MILLOT <millotflo@gmail.com>

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -13,6 +13,7 @@ import {
     Box,
     Button,
     ClickAwayListener,
+    darken,
     IconButton,
     ListItemIcon,
     ListItemText,
@@ -40,8 +41,7 @@ import {
     Settings as SettingsIcon,
     WbSunny as WbSunnyIcon,
 } from '@mui/icons-material';
-import { darken, styled } from '@mui/system';
-
+import { styled } from '@mui/system';
 import PropTypes from 'prop-types';
 import FullScreen, { fullScreenSupported } from 'react-request-fullscreen';
 
