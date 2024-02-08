2f008f0fbc31e8899b7822d28f3c1a54c49854d3
AAJELLAL <128486125+AAJELLAL@users.noreply.github.com>
2024-02-08 15:08:44 +0100

    Add security analysis and loadflow parameters icon. (#346)
    Signed-off-by: AAJELLAL <ali.ajellalod@gmail.com>

--- src/utils/ElementType.js
+++ src/utils/ElementType.js
@@ -18,6 +18,8 @@ export const elementType = {
     MODIFICATION: 'MODIFICATION',
     CONTINGENCY_LIST: 'CONTINGENCY_LIST',
     VOLTAGE_INIT_PARAMETERS: 'VOLTAGE_INIT_PARAMETERS',
+    SECURITY_ANALYSIS_PARAMETERS: 'SECURITY_ANALYSIS_PARAMETERS',
+    LOADFLOW_PARAMETERS: 'LOADFLOW_PARAMETERS',
 };
 
 export function getFileIcon(type, style) {
@@ -31,6 +33,8 @@ export function getFileIcon(type, style) {
         case elementType.FILTER:
             return <ArticleIcon sx={style} />;
         case elementType.VOLTAGE_INIT_PARAMETERS:
+        case elementType.SECURITY_ANALYSIS_PARAMETERS:
+        case elementType.LOADFLOW_PARAMETERS:
             return <SettingsIcon sx={style} />;
         case elementType.DIRECTORY:
             // to easily use in TreeView we do not give icons for directories
