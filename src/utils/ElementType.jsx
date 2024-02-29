c757b2b211bf44d6c9aafd180846904c59597ee2
dbraquart <107846716+dbraquart@users.noreply.github.com>
2024-02-29 19:06:54 +0100

    Use proper icon for study element (#362)
    Signed-off-by: David BRAQUART <david.braquart@rte-france.com>

--- src/utils/ElementType.jsx
+++ src/utils/ElementType.jsx
@@ -7,7 +7,8 @@
 import React from 'react';
 import {
     Article as ArticleIcon,
-    LibraryBooksOutlined as LibraryBooksOutlinedIcon,
+    PhotoLibrary as PhotoLibraryIcon,
+    Photo as PhotoIcon,
     OfflineBolt as OfflineBoltIcon,
     Settings as SettingsIcon,
     NoteAlt as NoteAltIcon,
@@ -16,6 +17,7 @@ import {
 export const elementType = {
     DIRECTORY: 'DIRECTORY',
     STUDY: 'STUDY',
+    CASE: 'CASE',
     FILTER: 'FILTER',
     MODIFICATION: 'MODIFICATION',
     CONTINGENCY_LIST: 'CONTINGENCY_LIST',
@@ -28,7 +30,9 @@ export const elementType = {
 export function getFileIcon(type, style) {
     switch (type) {
         case elementType.STUDY:
-            return <LibraryBooksOutlinedIcon sx={style} />;
+            return <PhotoLibraryIcon sx={style} />;
+        case elementType.CASE:
+            return <PhotoIcon sx={style} />;
         case elementType.CONTINGENCY_LIST:
             return <OfflineBoltIcon sx={style} />;
         case elementType.MODIFICATION:
@@ -38,6 +42,7 @@ export function getFileIcon(type, style) {
         case elementType.VOLTAGE_INIT_PARAMETERS:
         case elementType.SECURITY_ANALYSIS_PARAMETERS:
         case elementType.LOADFLOW_PARAMETERS:
+        case elementType.SENSITIVITY_PARAMETERS:
             return <SettingsIcon sx={style} />;
         case elementType.DIRECTORY:
             // to easily use in TreeView we do not give icons for directories
