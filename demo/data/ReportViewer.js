b86f88abdef25468d21531ed0e8c0ea6662e3d67
sBouzols <sylvain.bouzols@gmail.com>
2024-03-25 16:56:42 +0100

    fix
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- demo/data/ReportViewer.js
+++ demo/data/ReportViewer.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-template-curly-in-string */
-/*
+/**
  * Copyright (c) 2021, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
